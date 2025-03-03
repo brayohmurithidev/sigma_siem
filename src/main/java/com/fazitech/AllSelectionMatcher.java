bd6691097452f437231752ad1f7e7616b8275cba
brayohmurithidev <murithibrianm@gmail.com>
2025-03-03 09:48:39 +0300

    updated all selections condition

--- src/main/java/com/fazitech/AllSelectionMatcher.java
+++ src/main/java/com/fazitech/AllSelectionMatcher.java
@@ -1,4 +1,19 @@
 package com.fazitech;
 
-public class AllSelectionMatcher {
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class AllSelectionMatcher implements ConditionMatcher {
+    @Override
+    public boolean matches(JsonNode event, JsonNode detection) {
+        for (java.util.Iterator<String> it = detection.fieldNames(); it.hasNext(); ) {
+            String fieldName = it.next();
+            if (fieldName.startsWith("selection_")) {
+                JsonNode selection = detection.path(fieldName);
+                if (!new SelectionMatcher().checkSelection(event, selection)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
